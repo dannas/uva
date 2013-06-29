a9ec1bee4b485a79f5e745534c99c67d4643d383
Daniel Näslund <dannas@dannas.name>
2013-06-29 20:08:39 +0200

    10055 - hashmat the brave warrior

--- 10055.cc
+++ 10055.cc
@@ -1,14 +1,22 @@
+// 10055 - Hashmat the brave warrior
+
+// < 10 12
+// < 10 14
+// < 100 200
+
+// > 2
+// > 4
+// > 100
+
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
-int main(int argc, const char *argv[])
+int main()
 {
     long i, j;
 
     while (cin >> i >> j)
         cout << labs(i - j) << "\n";
-
-    return 0;
 }
