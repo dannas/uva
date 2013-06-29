761d19f2e1814814548df9ac1dfb6625dec3920e
Daniel Näslund <dannas@dannas.name>
2013-06-29 21:47:12 +0200

    156 - ananagrams

--- 156.cc
+++ 156.cc
@@ -1,3 +1,18 @@
+// 156 - Ananagrams
+
+// < ladder came tape soon leader acme RIDE lone Dreis peat
+// <  ScAlE orb  eye  Rides dealer  NotE derail LaCeS  drIed
+// < noel dire Disk mace Rob dries
+// < #
+
+// > Disk
+// > NotE
+// > derail
+// > drIed
+// > eye
+// > ladder
+// > soon
+
 #include <iostream>
 #include <map>
 #include <vector>
@@ -9,7 +24,7 @@ using namespace std;
 typedef multimap<string, string> Map;
 typedef vector<string> Vector;
 
-int main(int argc, const char *argv[])
+int main()
 {
     string word;
     Map m;
@@ -32,6 +47,4 @@ int main(int argc, const char *argv[])
 
     for (j = v.begin(); j != v.end(); ++j)
         cout << *j << "\n";
-
-    return 0;
 }
