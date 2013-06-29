0089664d047452c0c0650271789d2b85695e97ab
Daniel Näslund <dannas@dannas.name>
2013-06-29 15:27:01 +0200

    146 - id codes

---
+++ 146.cc
@@ -0,0 +1,42 @@
+// 145 - ID Codes
+
+// < abaacb
+// < cbbaa
+// < ab
+// < ba
+// < a
+// < abc
+// < acb
+// < bac
+// < cab
+// < cba
+// < #
+
+// > ababac
+// > No Successor
+// > ba
+// > No Successor
+// > No Successor
+// > acb
+// > bac
+// > bca
+// > cba
+// > No Successor
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string word;
+
+    while (cin >> word && word != "#") {
+        if (!next_permutation(word.begin(), word.end()))
+            cout << "No Successor\n";
+        else
+            cout << word << "\n";
+    }
+    return 0;
+}
